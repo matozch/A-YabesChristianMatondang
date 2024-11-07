f7e7a11c0d760d6c79680630c711bb4a3cfa02c8
Yabes Christian Matondang <162439966+matozch@users.noreply.github.com>
2024-11-07 22:48:20 +0700

    Create script.js

---
+++ Javascript Fundamental/script.js
@@ -0,0 +1,28 @@
+let name = "Andi";
+let age = 20;
+let isStudent = true;
+
+console.log("Nama:", name);
+console.log("Umur:", age);
+console.log("Mahasiswa:", isStudent);
+
+let animal = ["spider", "cat", "dog"];
+alert(animal[0]);
+alert(animal[1]);
+alert(animal[2]);
+
+let animals = ["Spider", "Cat", "dog"]
+alert(animals);
+alert(animals.length);
+
+const person = {firstName:"John", LastName:"Matondang", Age:"22", eyeColor:"blue"};
+console.log(person);
+
+let a = 10;
+let b = 12;
+console.log(a < 15 && b > 10); 
+console.log(a > 15 && b > 10); 
+console.log(a < 15 || b > 10); 
+console.log(a > 15 || b > 10); 
+console.log(!(a < 15)); 
+console.log(!(a < 15 && b > 10)); 
